f0ec0f3e3f6e5445d3702067eef81091fe27463c
Doguhan Yangoz <doguhanyangoz@gmail.com>
2021-10-30 11:30:49 +0200

    Header component has been implemented.

---
+++ src/components/app/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import TodoHeader from '../header/Header';
+
+export function App () {
+  return (
+   <div className="App">
+                <TodoHeader name={"Doguhan Michi"}/>
+            </div>
+  )
+}
